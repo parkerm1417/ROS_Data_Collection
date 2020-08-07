b16e867d7386c26ba3c672a7066a6e01ab35a02e
parkera1476 <67754150+parkera1476@users.noreply.github.com>
2020-08-07 11:45:13 -0500

    Update services.py

--- services.py
+++ services.py
@@ -4,7 +4,12 @@ import time
 import json
 from datetime import date
 
-
+################################################################################
+##This function gets all of the services that are provided by the packages in
+##the complete dictionary and then adds those services to a separate dictionary
+##of its own. This dictionary holds all of the services for the packages that
+##were asked for by the user. This function is called in RTKpackages.py.
+################################################################################
 def serviceFunc(packages,distro,inputName,Complete):
     primitives = ["byte","bool","int8","uint8","int16","uint16","int32","uint32","int64","uint64","float32","float64","string","time","duration","bool[]","int8[]","uint8[]","int16[]","uint16[]","int32[]","uint32[]","int64[]","uint64[]","float32[]","float64[]","string[]","time[]","duration[]", "float64[4]", "float64[9]", "float64[12]","float64[36]"]
 
@@ -17,6 +22,7 @@ def serviceFunc(packages,distro,inputName,Complete):
     except:
         msgdic = {}
 
+##Gets the services provided from the packages in the complete dictionary and stores it in a list "edits"
     for pack in Complete:
         if pack != "Distribution" and pack != "Date":
             for node in Complete[pack]["nodes"]:
@@ -24,7 +30,9 @@ def serviceFunc(packages,distro,inputName,Complete):
                     if Complete[pack]["nodes"][node]["services provided"][serv]["type"] not in edits:
                         edits.append(Complete[pack]["nodes"][node]["services provided"][serv]["type"])
     c=1
-    
+
+##If the service is not already in the dictionary, then the service gets added. This ensures that the
+##same service isn't added more than once.
     for msg in edits:
         if msg not in msgdic:
             bs = msg.find("/")
@@ -35,13 +43,15 @@ def serviceFunc(packages,distro,inputName,Complete):
                 "responseMessage": msg[bs+1:]+"Response"
                 }
             print("Service "+ str(c) +" of "+ str(len(edits)) +" completed")
-
+            ##Adds on to a temporary dictionary to ensure that if the code crashes or is stopped
+            ##that the information will be saved and the code will be able to pick up where it left off
             file = open("tempServices.json","w")
             file.write(json.dumps(msgdic))
             file.close()
             
         c+=1
-
+##Creates a file using the user input as a part of the name, as well as the date.
+##This file gets added to the current directory of the user.        
     filename =  inputName + "_srvs_" + date.today().strftime("%m_%d_%Y") + ".json"
     srvs = open(filename,"w")
     srvs.write(json.dumps(msgdic, indent = 8))
