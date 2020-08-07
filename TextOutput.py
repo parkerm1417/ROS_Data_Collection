86f5203e4098add135d11a5dfeea1ac28a7b23a6
parkera1476 <67754150+parkera1476@users.noreply.github.com>
2020-08-07 10:56:50 -0500

    Update TextOutput.py

--- TextOutput.py
+++ TextOutput.py
@@ -4,6 +4,12 @@ from os import system
 
 primitives = ["byte","bool","int8","uint8","int16","uint16","int32","uint32","int64","uint64","float32","float64","string","time","duration","bool[]","int8[]","uint8[]","int16[]","uint16[]","int32[]","uint32[]","int64[]","uint64[]","float32[]","float64[]","string[]","time[]","duration[]", "float64[4]", "float64[9]", "float64[12]","float64[36]","unknown type","char"]
 
+########################################################################
+##Gets input from the user of what dictionaries they want to get
+##information from. Both the packages and messages dictionaries are
+##necessary. Those dictionaries get read and loaded from the json form
+##that they are put into from RTKpackages.py.
+########################################################################
 me = sp.getoutput("whoami")
 print("The file names you are about to enter should\nall come from the same run of RTKpackages.py")
 packagedict = input("Enter the exact file path of the package dictionary .json\nfile you wish to get output from.\nExample: /home/mie/Desktop/packages.json\n")
@@ -22,6 +28,17 @@ file.close()
 
 messages = json.loads(msgs)
 
+########################################################################
+##First creates a directory named Architecture_Documents on the desktop
+##using the terminal command mkdir. Then within that directory, two
+##folders are made named ROS and RTK. These folders will hold the
+##package folders which are organized based on whether they are part
+##of the ROS or RTK build. Each node has a .txt file created for it
+##that has the documentation of the node. The documentation is
+##formatted to easily be inserted into a multilevel list on Microsoft
+##Word.
+########################################################################
+
 system("mkdir /home/" + me + "/Desktop/Architecture_Documents")
 system("mkdir /home/" + me + "/Desktop/Architecture_Documents/ROS")
 system("mkdir /home/" + me + "/Desktop/Architecture_Documents/RTK")
@@ -29,6 +46,11 @@ c = 0
 for pack in bigdict:
     if pack == "Distribution" or pack == "Date":
         continue
+
+########################################################################
+##This section is for creating the ROS files by checking if the
+##distribution of the package is ROS.
+########################################################################
     if (bigdict[pack]["distribution"][:3] == "ROS"):
         system("mkdir /home/" + me + "/Desktop/Architecture_Documents/ROS/" + pack)
         for node in bigdict[pack]["nodes"]:
@@ -80,7 +102,10 @@ for pack in bigdict:
                     for com in messages[msg]["comments"]:
                         file.write("                              " + messages[msg]["comments"][com] + "\n")
                     used.append(msg)
-
+########################################################################
+##This section is for creating the RTK files by checking if the
+##distribution of the package is RTK.
+########################################################################
     elif (bigdict[pack]["distribution"][:3] == "RTK"):
         system("mkdir /home/" + me + "/Desktop/Architecture_Documents/RTK/" + pack)
         for node in bigdict[pack]["nodes"]:
